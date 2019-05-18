ccfbb037d4b5c36d0ada37c3a4dfb9cf4bde488c
Olivia Yang <yang4862@umn.edu>
2019-05-18 11:11:09 -0500

    added navbar

---
+++ create-react-express/client/src/components/NavBar/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Nav } from "react-bootstrap";
+import"./nav.css";
+
+function NavBar() {
+  return (
+    <Nav
+      activeKey="/home"
+      onSelect={selectedKey => alert(`selected ${selectedKey}`)}
+      className="NavBar"
+    >
+      <Nav.Item >
+        <Nav.Link className="logo"href="/home">Dinder<i className="fas fa-bone fa-sm"></i></Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-1">Search</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-2">Update Profile</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-3">Create a Dog</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-4">Start a Chat</Nav.Link>
+      </Nav.Item>
+      
+    </Nav>
+  );
+}
+
+export default NavBar;
--- create-react-express/client/src/pages/PageTwo.js
+++ create-react-express/client/src/pages/PageTwo.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Show from "../components/Show";
+import NavBar from "../components/NavBar";
 import { Container, Row, Col } from "react-bootstrap";
 import "./PageTwo.css";
 
@@ -7,16 +8,17 @@ class PageTwo extends Component {
   render() {
     return (
       <Container>
+        <NavBar></NavBar>
         <Show></Show>
         <Row>
           <Col>
-          <p>column one</p>
+            <p>column one</p>
           </Col>
           <Col>
-          <p>column two</p>
+            <p>column two</p>
           </Col>
           <Col>
-          <p>column three</p>
+            <p>column three</p>
           </Col>
         </Row>
       </Container>
