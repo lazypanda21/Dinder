f3f5ab2746b763ce74de9db095f20276149c96fd
Olivia Yang <yang4862@umn.edu>
2019-05-19 20:00:12 -0500

    push

--- create-react-express/client/src/utils/API.js
+++ create-react-express/client/src/utils/API.js
@@ -1,19 +1,7 @@
 import axios from "axios";
 
 export default {
-  // Gets all books
-  getBooks: function() {
-    return axios.get("/api/books");
-  },
-  // Gets the book with the given id
-  getBook: function(id) {
-    return axios.get("/api/books/" + id);
-  },
-  // Deletes the book with the given id
-  deleteBook: function(id) {
-    return axios.delete("/api/books/" + id);
-  },
-  // Saves a book to the database
+  // Saves a user to the ownerlogin
   saveUser: function(userData) {
     return axios.post("api/Login/create", userData);
   },
@@ -29,7 +17,7 @@ export default {
   },
 
   updateOwner: function(username){
-    return axios.post("api/Owner", username);
+    return axios.put("api/Owner", username);
   },
   // get dogs by breed
   getDogs :function(dogbreeds){
